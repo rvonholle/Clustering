bd0018d75397d90fdd69e48e7144cdffbe14ad34
Robert Von Holle <rvonholle@ara.com>
2018-07-06 13:06:17 -0400

    The epic k-means debug part 3

--- natural_clustering/2D_clustering/k_means_clustering.f90
+++ natural_clustering/2D_clustering/k_means_clustering.f90
@@ -1,16 +1,16 @@
 program k_means_clustering
    implicit none
    type Point
-      real :: x_coord
-      real :: y_coord
+      real :: x_coord = 0.0
+      real :: y_coord = 0.0
    end type Point
    type Cluster
       type(Point) :: center
       type(Point), allocatable :: clusterPoints(:)
    end type Cluster
    type(Point), allocatable :: dataPoints(:)
-   type(Point), dimension(:,:), allocatable :: clusters(:,:)
-   type(Point), allocatable :: tempList(:)
+   type(Cluster), allocatable :: clusters(:)
+   type(Cluster) :: tempList
    type(Point) :: origin
    character(len=100) :: dataFile, clusteredFile
    character(len=1) :: chooseClusters
@@ -83,18 +83,21 @@ program k_means_clustering
          end do
       end do
    end if
-   allocate(clusters(numClusters+1,numData+1))
-   allocate(tempList(numData+1))
-   clusters(:,:)%x_coord = 0
-   clusters(:,:)%y_coord = 0
-   tempList(:)%x_coord = 0
-   tempList(:)%y_coord = 0
+   allocate(clusters(numClusters))
+   do i = 1, numClusters
+      allocate(clusters(i)%clusterPoints(numData))
+   end do
+   allocate(tempList%clusterPoints(numData))
+!   clusters(:)%clusterPoints(:)%x_coord = 0
+!   clusters(:)%clusterPoints(:)%y_coord = 0
+!   tempList%clusterPoints(:)%x_coord = 0
+!   tempList%clusterPoints(:)%y_coord = 0
 
 ! Cluster the points
    if(dataPoints(1)%x_coord/=dataPoints(numData)%x_coord.or.dataPoints(1)%y_coord/=dataPoints(numData)%y_coord)then
       temp = nint(numData_copy / numClusters_copy)
       do i = 1, numClusters
-         clusters(i,1) = dataPoints(1 + (temp * (i - 1)))
+         clusters(i)%center = dataPoints(1 + (temp * (i - 1)))
       end do
       do j = 1, 50
          call pickCenters(dataPoints, clusters, numData, numClusters)
@@ -102,8 +105,6 @@ program k_means_clustering
             call setTempList(clusters, tempList, numData, i)
             call findCentroid(tempList, numData)
             call resetCluster(clusters, tempList, numData, i)
-         end do
-         do i = 1, numData
          end do
       end do
    end if
@@ -115,14 +116,14 @@ program k_means_clustering
       write (2, *) "Cluster 1:"
       do i = 1, numData
          if (abs(dataPoints(1)%x_coord) < 0.1 .or. abs(dataPoints(1)%x_coord) > 1e7) then
-            write (2, '(A1,ES13.2,A1)', Advance = 'No') "(", dataPoints(i)%x_coord, ","
+            write (2, '(A1,ES13.7,A1)', Advance = 'No') "(", dataPoints(i)%x_coord, ","
          else
-            write (2, '(A1,F12.2,A1)', Advance = 'No') "(", dataPoints(i)%x_coord, ","
+            write (2, '(A1,F12.7,A1)', Advance = 'No') "(", dataPoints(i)%x_coord, ","
          end if
          if (abs(dataPoints(1)%y_coord) < 0.1 .or. abs(dataPoints(1)%y_coord) > 1e7) then
-            write (2, '(ES13.2,A1)') dataPoints(i)%y_coord, ")"
+            write (2, '(ES13.7,A1)') dataPoints(i)%y_coord, ")"
          else
-            write (2, '(F12.2,A1)') dataPoints(i)%y_coord, ")"
+            write (2, '(F12.7,A1)') dataPoints(i)%y_coord, ")"
          end if
       end do
    else
@@ -137,23 +138,23 @@ program k_means_clustering
          end if
          call setTempList(clusters, tempList, numData, i)
          k = 0
-         do j = 2, numData
-            if (tempList(j)%x_coord /= 0 .and. tempList(j)%y_coord /= 0) then
+         do j = 1, numData
+            if (tempList%clusterPoints(j)%x_coord /= 0 .and. tempList%clusterPoints(j)%y_coord /= 0) then
                k = k + 1
             end if
          end do
          if (k > 0) then
-            do j = 2, numData
-               if (tempList(j)%x_coord /= 0 .and. tempList(j)%y_coord /= 0) then
-                  if (abs(tempList(j)%x_coord) < 0.1 .or. abs(tempList(j)%x_coord) >= 1e7) then
-                     write (2, '(A1,ES16.2,A1)', Advance = 'No') "(", tempList(j)%x_coord, ","
+            do j = 1, numData
+               if (tempList%clusterPoints(j)%x_coord /= 0 .and. tempList%clusterPoints(j)%y_coord /= 0) then
+                  if(abs(tempList%clusterPoints(j)%x_coord)<0.1.or.abs(tempList%clusterPoints(j)%x_coord)>=1e7)then
+                     write (2, '(A1,ES16.7,A1)', Advance = 'No') "(", tempList%clusterPoints(j)%x_coord, ","
                   else
-                     write (2, '(A1,F12.2,A1)', Advance = 'No') "(", tempList(j)%x_coord, ","
+                     write (2, '(A1,F12.7,A1)', Advance = 'No') "(", tempList%clusterPoints(j)%x_coord, ","
                   end if
-                  if (abs(tempList(j)%y_coord) < 0.1 .or. abs(tempList(j)%y_coord) >= 1e7) then
-                     write (2, '(ES16.2,A1)') tempList(j)%y_coord, ")"
+                  if(abs(tempList%clusterPoints(j)%y_coord)<0.1.or.abs(tempList%clusterPoints(j)%y_coord)>=1e7)then
+                     write (2, '(ES16.7,A1)') tempList%clusterPoints(j)%y_coord, ")"
                   else
-                     write (2, '(F12.2,A1)') tempList(j)%y_coord, ")"
+                     write (2, '(F12.7,A1)') tempList%clusterPoints(j)%y_coord, ")"
                   end if
                end if
             end do
@@ -189,32 +190,30 @@ end subroutine sortPoints
 subroutine pickCenters(dataPoints, clusters, numData, numClusters)
    implicit none
    type(Point), intent(in) :: dataPoints(:)
-   type(Point) :: clusters(:,:)
+   type(Cluster) :: clusters(:)
    integer, intent(in) :: numData
    integer, intent(in) :: numClusters
    real :: centerDist(numClusters)
    integer i, j, m, n
    do i = 1, numClusters
-      do j = 2, numData
-         clusters(i,j)%x_coord = 0
-         clusters(i,j)%y_coord = 0
-      end do
+      clusters(i)%clusterPoints(:)%x_coord = 0
+      clusters(i)%clusterPoints(:)%y_coord = 0
    end do
    do i = 1, numData
       centerDist(:) = 0
       do j = 1, numClusters
-         centerDist(j) = dist(dataPoints(i), clusters(j,1))
+         centerDist(j) = dist(dataPoints(i), clusters(j)%center)
       end do
       m = minloc(centerDist, dim=1)
       n = 1
-      do while (clusters(m,n)%x_coord /= 0 .and. clusters(m,n)%y_coord /= 0)
+      do while (clusters(m)%clusterPoints(n)%x_coord /= 0 .and. clusters(m)%clusterPoints(n)%y_coord /= 0)
          n = n + 1
          if (n > numData + 1) then
             exit
          end if
       end do
       if (n <= numData + 1) then
-         clusters(m,n) = dataPoints(i)
+         clusters(m)%clusterPoints(n) = dataPoints(i)
       end if
    end do
 end subroutine pickCenters
@@ -232,37 +231,36 @@ end function dist
 ! This subroutine finds the centroid of a cluster
 subroutine findCentroid(currCluster, numData)
    implicit none
-   type(Point) :: currCluster(:)
+   type(Cluster) :: currCluster
    integer :: numData
    integer :: i, nonOrigin
-   currCluster(1)%x_coord = 0
-   currCluster(1)%y_coord = 0
+   currCluster%center%x_coord = 0
+   currCluster%center%y_coord = 0
    nonOrigin = 0
    do i = 1, numData
-      currCluster(1)%x_coord = currCluster(1)%x_coord + currCluster(i+1)%x_coord
-      currCluster(1)%y_coord = currCluster(1)%y_coord + currCluster(i+1)%y_coord
-      if (currCluster(i+1)%x_coord /= 0 .and. currCluster(i+1)%y_coord /= 0) then
+      currCluster%center%x_coord = currCluster%center%x_coord + currCluster%clusterPoints(i)%x_coord
+      currCluster%center%y_coord = currCluster%center%y_coord + currCluster%clusterPoints(i)%y_coord
+      if (currCluster%clusterPoints(i)%x_coord /= 0 .and. currCluster%clusterPoints(i)%y_coord /= 0) then
          nonOrigin = nonOrigin + 1
       end if
    end do
-   currCluster(1)%x_coord = currCluster(1)%x_coord / nonOrigin
-   currCluster(1)%y_coord = currCluster(1)%y_coord / nonOrigin
+   currCluster%center%x_coord = currCluster%center%x_coord / nonOrigin
+   currCluster%center%y_coord = currCluster%center%y_coord / nonOrigin
 end subroutine findCentroid
 
 ! This subroutine sets tempList equal to the current cluster
 subroutine setTempList(clusters, tempList, numData, clusterID)
    implicit none
-   type(Point) :: clusters(:,:)
-   type(Point) :: tempList(:)
+   type(Cluster) :: clusters(:)
+   type(Cluster) :: tempList
    integer :: numData, clusterID
    integer :: i
-   do i = 1, numData + 1
-      tempList(i)%x_coord = 0
-      tempList(i)%y_coord = 0
-   end do
-   do i = 1, numData + 1
-      if (clusters(clusterID,i)%x_coord /= 0 .and. clusters(clusterID,i)%y_coord /= 0) then
-         tempList(i) = clusters(clusterID,i)
+   tempList%clusterPoints(:)%x_coord = 0
+   tempList%clusterPoints(:)%y_coord = 0
+   tempList%center = clusters(clusterID)%center
+   do i = 1, numData
+      if(clusters(clusterID)%clusterPoints(i)%x_coord/=0.and.clusters(clusterID)%clusterPoints(i)%y_coord/= 0)then
+         tempList%clusterPoints(i) = clusters(clusterID)%clusterPoints(i)
       end if
    end do
 end subroutine setTempList
@@ -270,17 +268,15 @@ end subroutine setTempList
 ! This subroutine sets the current cluster equal to tempList
 subroutine resetCluster(clusters, tempList, numData, clusterID)
    implicit none
-   type(Point) :: clusters(:,:)
-   type(Point) :: tempList(:)
+   type(Cluster) :: clusters(:)
+   type(Cluster) :: tempList
    integer :: numData, clusterID
    integer :: i
-   do i = 2, numData + 1
-      clusters(clusterID,i)%x_coord = 0
-      clusters(clusterID,i)%y_coord = 0
-   end do
-   do i = 1, numData + 1
-      if (tempList(i)%x_coord /= 0 .and. tempList(i)%y_coord /= 0) then
-         clusters(clusterID,i) = tempList(i)
+      clusters(clusterID)%clusterPoints(:)%x_coord = 0
+      clusters(clusterID)%clusterPoints(:)%y_coord = 0
+   do i = 1, numData
+      if (tempList%clusterPoints(i)%x_coord /= 0 .and. tempList%clusterPoints(i)%y_coord /= 0) then
+         clusters(clusterID)%clusterPoints(i) = tempList%clusterPoints(i)
       end if
    end do
 end subroutine resetCluster
